b4c966554ed1059fa48064bbceea35bb7d33d3c1
Aditya Gupta <guptasoftworks@gmail.com>
2025-09-13 20:10:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToDetection(){
+      window.location.href = "mini.html";
+    }
+function uploadImage(){
+      alert("Image upload detection coming soon 🚀");
+    }
+function showHistory(){
+      alert("Recent history will appear here.");
+    }
